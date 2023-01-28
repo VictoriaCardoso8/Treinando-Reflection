5c18d4746fca58e29ec6a77207efc540c63b9a1e
Git Test <tori.cardoso@hotmail.com>
2023-01-28 11:54:43 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 5/Exercicio 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+//Crie um método que receba um objeto genérico e liste as propriedades e métodos dele
+
+//Crie um método que receba um objeto genérico e crie uma instância dele e a retorne
+
+
+ListData(new Pessoa());
+void ListData<T>(T obj)
+{
+    var objType = obj.GetType();
+    var properties = objType.GetProperties();
+    foreach(var item in properties)
+    {
+        Console.WriteLine($"{item.Name} - {item.GetValue(obj)}");
+    }
+    var methods = objType.GetMethods();
+    foreach(var item in methods)
+    {
+        Console.WriteLine($"{item.Name} - {item.ReturnType}");
+    }
+}
+T CreatObject<T>()
+{
+    return Activator.CreateInstance<T>();
+}
+
+class Pessoa
+{
+    public int Nome { get; set; }
+    public double ID { get; set; }
+}
